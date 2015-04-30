cd3d81441a9fb0475e924e8c802f80fa0e505507
Zac-King <thebrokekiller@yahoo.com>
2015-04-30 16:35:45 -0500

    init commit

---
+++ Assets/Scripts/TurnOn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOn : MonoBehaviour {
+
+	private int increment = 0;
+
+	void Start () {
+		//Debug.Log("I'm in start.");
+
+	}
+
+	void Update () {
+
+		//Debug.Log("Update:" + increment);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			increment++;
+		}
+	}
+
+	private void Awake () {
+		//Debug.Log ("Is Active");
+	}
+
+}
